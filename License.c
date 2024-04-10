05955fe2fd958712fd7358bce2286bc3d18f82fa
JURKFLY <165411207+JURKFLY@users.noreply.github.com>
2024-04-11 00:50:57 +0200

    Update License.c
    V2

--- License.c
+++ License.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h> // Pour utiliser la fonction system()
 
 int main() {
     printf("Licence d'utilisation du système FireOs\n\n");
     printf("Ce logiciel est protégé par les lois sur le droit d'auteur et est la propriété de FireOs. Toute reproduction, modification, distribution ou utilisation non autorisée du code source ou du logiciel est strictement interdite et peut entraîner des poursuites judiciaires. En installant, copiant ou utilisant ce logiciel, vous acceptez les termes de cette licence.\n\n");
     printf("FireOs se réserve tous les droits non expressément accordés dans cette licence.\n\n");
     printf("Date: [Jeudi 11 Avril 2024]\n\n");
+    system("sleep 5"); // Utilisation de la fonction system() avec la commande sleep
     return 0;
 }
 // Language:C 
