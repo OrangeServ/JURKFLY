446eea0620b1d8718badb9fa2ef3f06f95a6b239
JURKFLY <165411207+JURKFLY@users.noreply.github.com>
2024-04-11 00:32:51 +0200

    License.c
    V2

--- License.c
+++ License.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Licence d'utilisation du logiciel FireOs en C :\n\n");
+    printf("Licence d'utilisation du système FireOs\n\n");
     printf("Ce logiciel est protégé par les lois sur le droit d'auteur et est la propriété de FireOs. Toute reproduction, modification, distribution ou utilisation non autorisée du code source ou du logiciel est strictement interdite et peut entraîner des poursuites judiciaires. En installant, copiant ou utilisant ce logiciel, vous acceptez les termes de cette licence.\n\n");
     printf("FireOs se réserve tous les droits non expressément accordés dans cette licence.\n\n");
-    printf("Date: [Insérer la date de la licence]\n\n");
+    printf("Date: [Jeudi 11 Avril 2024]\n\n");
     return 0;
 }
+// Language:C 
+// Copy the full code and open the CCoder APP to run it. 
+// CCoder APP download link：https://play.google.com/store/apps/details?id=com.ikou.ccoding 
