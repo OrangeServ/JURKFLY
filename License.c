3b06d25bf6eb39705d51fcb9e7e613f31fbd459d
JURKFLY <165411207+JURKFLY@users.noreply.github.com>
2024-04-11 00:28:48 +0200

    Update 1
    License of FireOs

---
+++ License.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Licence d'utilisation du logiciel FireOs en C :\n\n");
+    printf("Ce logiciel est protégé par les lois sur le droit d'auteur et est la propriété de FireOs. Toute reproduction, modification, distribution ou utilisation non autorisée du code source ou du logiciel est strictement interdite et peut entraîner des poursuites judiciaires. En installant, copiant ou utilisant ce logiciel, vous acceptez les termes de cette licence.\n\n");
+    printf("FireOs se réserve tous les droits non expressément accordés dans cette licence.\n\n");
+    printf("Date: [Insérer la date de la licence]\n\n");
+    return 0;
+}
