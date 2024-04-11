aa4bc9e4e89428ac264142623f0aaceb10c1dc9c
JURKFLY <165411207+JURKFLY@users.noreply.github.com>
2024-04-11 21:55:03 +0200

    Update démarrage.c
    V3

--- démarrage.c
+++ démarrage.c
@@ -1,29 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <stdlib.h> // Pour utiliser la fonction system()
 
 void demarrage() __attribute__((constructor));
 
 void demarrage() {
-    printf("Le système démarre...\n");
-    // Autres actions de démarrage à ajouter si nécessaire
 
-    char bar[] = "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
-    int i;
-    printf("Chargement de FireOs: ");
-    for (i = 0; i <= 50; i++) {
-        printf("\r");
-        printf("[%-50s] %d%%", bar + i, 2 * i);
-        fflush(stdout);
-        usleep(100000); // ajustez ce délai selon votre préférence
-    }
-    printf("\nFireOs chargé avec succès!\n");
-    sleep(2.5);
-    system("clear"); // Efface l'écran après le chargement
+int i;
+  for (i = 0; i <= 0.5; i++) {
+    printf("\F");
+    fflush(stdout);
+    usleep(1000000); // ajustez ce délai selon votre préférence
+  }
+  for (i = 0; i <= 0.5; i++) {
+    printf("\i");
+    fflush(stdout);
+    usleep(1000000); // ajustez ce délai selon votre préférence
+  } 
+  for (i = 0; i <= 0.5; i++) {
+    printf("\r");
+    fflush(stdout);
+    usleep(1000000); // ajustez ce délai selon votre préférence
+  }
+    for (i = 0; i <=0.5; i++) {
+    printf("\e");
+    fflush(stdout);
+    usleep(1000000); // ajustez ce délai selon votre préférence
+  }
+  for (i = 0; i <= 1; i++) {
+    printf("\O");
+    fflush(stdout);
+    usleep(1000000); // ajustez ce délai selon votre préférence
+  }
+
+  for (i = 0; i <= 1; i++) {
+    printf("\s");
+    fflush(stdout);
+    usleep(1000000); // ajustez ce délai selon votre préférence
+  } 
+
+  for (i = 0; i <= 10; i++) {
+    printf("\⬜");
+    fflush(stdout);
+    usleep(1000000); // ajustez ce délai selon votre préférence
+  }
+  system("clear");
+  printf("\nFireOs chargé avec succès!\n");
+  sleep(2);        // Correction du délai de sleep
+  system("clear"); // Efface l'écran après le chargement
 }
 
-// Cette fonction est nécessaire pour éviter les avertissements sur une fonction sans appel.
+// Cette fonction est nécessaire pour éviter les avertissements sur une fonction
+// sans appel.
 void placeholder() {}
+
+
 // Language:C 
 // Copy the full code and open the CCoder APP to run it. 
 // CCoder APP download link：https://play.google.com/store/apps/details?id=com.ikou.ccoding 
